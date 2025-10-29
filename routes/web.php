57175921905bb8699ea26df04731b5c2f7795625
nhilmya2232 <naufalhilmya@gmail.com>
2025-10-29 12:28:54 +0700

    Reload Ulang dari Back End

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,37 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/test', function () {
+    return Inertia::render('test');
+})->name('test');
+
+Route::get('/landing', function () {
+    return Inertia::render('LandingPage');
+});
+
+Route::get('/reservasi', function () {
+    return Inertia::render('Reservasi');
+});
+
+Route::get('/test1', function () {
+    return Inertia::render('test1');
+});
+
+Route::get('/masuk', function () {
+    return Inertia::render('Register');
+});
+
+Route::get('/loh', function () {
+    return Inertia::render('Login');
+});
+
+Route::get('/order', function () {
+    return Inertia::render('Order');
+});
+
+Route::get('/admin-dashboard', function () {
+    return Inertia::render('AdminDashboard');
+});
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
